15a95f578df8a338751fec21fedb9217403794a6
NikitaNeti <nikitaneti19@gmail.com>
2022-12-29 13:48:37 +0530

    second commit

--- Web scraping/main.py
+++ Web scraping/main.py
@@ -1,7 +1,36 @@
 from bs4 import BeautifulSoup
 import requests
 
-result = requests.get("http://www.google.com")
-content = result.text
-soup = BeautifulSoup(content,"lxml")
-print(soup)
+# Fetch the pages
+# result = requests.get("http://www.google.com")
+
+#Get the page content
+# content = result.text
+
+#create the soup
+# soup = BeautifulSoup(content,"lxml")
+# print(soup.title)
+
+from bs4 import BeautifulSoup
+with open("index.html") as fp:
+    soup = BeautifulSoup(fp,'lxml')
+    soup = BeautifulSoup("<b>Extremely bold</b>","lxml")
+    print(type(soup))
+    print(soup.name)
+
+    tag = soup.b
+    print(type(tag))
+    print(tag.name)
+
+    tag.name = 'blockquote'
+    print(tag)
+
+    soup1 = BeautifulSoup("<p class='title'><b>The Dormouse's story</b></p>",'lxml')
+    tag2 = soup1.p
+    # print(tag2['class'])
+    print(soup1.string)
+    # print(type(soup1.string))
+    soup1.string.replace_with("hiiiiiiii")
+    print(tag2)
+
+
